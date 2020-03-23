ee881bbb48eb2073d1229ce764fabe7cfc5a29bc
alexdoublesmile <alexplohoy@mail.ru>
2020-03-23 19:20:26 +0300

    start heroku 5

--- src/main/java/com/plohoy/bulls/servlet/HomeServlet.java
+++ src/main/java/com/plohoy/bulls/servlet/HomeServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HomeServlet", urlPatterns = {"/", "/home"})
+@WebServlet(name = "HomeServlet", urlPatterns = {"/home", "/index"})
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
